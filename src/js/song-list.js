30fe0e944c808872e0e5dc705378005789ed8b6d
qinggge <zhuhuang@126.com>
2018-08-12 21:25:05 +0800

    click -> song active

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -10,12 +10,16 @@
             $el.html(this.template)
             let {songs} = data
             let liList = songs.map((song)=> 
-                $('<li></li>').text(song.name)
+                $('<li></li>').text(song.name).attr('data-song-id',song.id)
             )
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
+        },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active').siblings('.active').removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
@@ -24,6 +28,15 @@
     let model = {
         data: {
             songs: [ ]
+        },
+        find(){
+            var query = new AV.Query('Song');
+            return query.find().then((songs)=>{
+                this.data.songs = songs.map((song)=>{
+                    return {id:song.id, ...song.attributes}
+                })
+                return songs
+            })
         }
     }
 
@@ -32,10 +45,27 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
-            window.eventHub.on('upload',()=>{
+            this.bindEvents()
+            this.getAllSongs()
+            this.bindEventHub()
+        },
+        getAllSongs() {
+            this.model.find().then(() => {
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id:songId})
+            })
+        },
+        bindEventHub() {
+            window.eventHub.on('upload', () => {
                 this.view.clearActive()
             })
-            window.eventHub.on('create',(songData)=>{
+            window.eventHub.on('create', (songData) => {
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
             })
