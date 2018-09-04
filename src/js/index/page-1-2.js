e44072e5833d52600b419dba9483434cd3326a54
qinggge <zhu_huang@126.com>
2018-09-05 07:10:43 +0800

    添加歌曲播放暂停

--- src/js/index/page-1-2.js
+++ src/js/index/page-1-2.js
@@ -1,6 +1,22 @@
 {
     let view ={
         el: 'section.songs',
+        template: `
+        <li>
+            <h3>{{song.name}}</h3>
+            <p>
+                <svg class="icon icon-sq">
+                    <use xlink:href="#icon-sq"></use>
+                </svg>
+                {{song.singer}}
+            </p>
+            <a class="playButton" href="./song.html?id={{song.id}}">
+                <svg class="icon icon-play">
+                    <use xlink:href="#icon-play"></use>
+                </svg>
+            </a>
+        </li>
+        `,
         init(){
             this.$el = $(this.el)
         },
@@ -8,22 +24,11 @@
             let {songs} = data
             console.log(songs)
             songs.map((song)=>{
-                let $li = $(`
-                <li>
-                    <h3>${song.name}</h3>
-                    <p>
-                        <svg class="icon icon-sq">
-                            <use xlink:href="#icon-sq"></use>
-                        </svg>
-                        ${song.singer}
-                    </p>
-                    <a class="playButton" href="#">
-                        <svg class="icon icon-play">
-                            <use xlink:href="#icon-play"></use>
-                        </svg>
-                    </a>
-                </li>
-                `)
+                let $li = $(this.template
+                .replace('{{song.name}}',song.name)
+                .replace('{{song.singer}}',song.singer)
+                .replace('{{song.id}}',song.id)
+            )
                 this.$el.find('ol.list').append($li)
             })
         }
