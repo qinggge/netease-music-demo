fdefed97ed6bf089757243f5dc9d9e2571dffe90
qinggge <zhu_huang@126.com>
2018-09-06 19:49:54 +0800

    修改js路径

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -86,7 +86,7 @@
         get(id){
             var query = new AV.Query('Song')
             return query.get(id).then((song)=>{
-                Object.assign(this.data.song,{id:song.id, ...song.attributes})
+                Object.assign(this.data.song,Object.assign({id:song.id},song.attributes))
                 return song
             })
         }
