60d746197f4781498f3962dee43fcb49cd08d885
qinggge <zhu_huang@126.com>
2018-09-11 10:47:33 +0800

    新建歌单、修改歌单

---
+++ src/js/admin-playlists/list-form.js
@@ -0,0 +1,107 @@
+    {
+        let view = {
+            el: '.playlistForm-wrapper',
+            init(){
+                this.$el = $(this.el)
+                this.$form = this.$el.find('form')
+            },
+            render(data = {}){
+                let name = document.querySelector('input[name="name"]')
+                let summary = document.querySelector('textarea[name="summary"]')
+                $(name).val(data.name)
+                $(summary).val(data.summary)
+            },
+            reset(){
+                this.render({})
+            }
+
+        }
+        let model = {
+            data:{
+                playlists: []
+            },
+            update(data){
+                var playlist = AV.Object.createWithoutData('Playlist', this.data.id)
+                playlist.set('name',data.name)
+                playlist.set('summary',data.summary)
+                return playlist.save().then((response)=>{
+                    Object.assign(this.data,data)
+                    return response
+                })
+            },
+            create(data){
+                var Playlist =  AV.Object.extend('Playlist')
+                var playlist = new Playlist()
+                playlist.set('name',data.name)
+                playlist.set('summary',data.summary)
+                return playlist.save().then((newPlaylist) => {
+                    let {
+                        id,
+                        attributes
+                    } = newPlaylist
+                    Object.assign(this.data, {
+                        id,
+                        ...attributes
+                    })
+                }, (error) => {
+                    console.error(error)
+                })
+            }
+        }
+        let controller = {
+            init(view,model){
+                this.view = view
+                this.view.init()
+                this.model = model
+                this.view.render(this.model.data)
+
+                this.bindEvents()
+                this.bindEventHub()
+            },
+            create(){
+                let needs ='name summary'.split(' ') 
+                let data = {}
+                needs.map((string) => {
+                    data[string] = this.view.$el.find(`[name="${string}"]`).val()
+                })
+                this.model.create(data)
+                    .then(() => {
+                        this.view.reset()
+                        let string = JSON.stringify(this.model.data)
+                        let object = JSON.parse(string)
+                        window.eventHub.emit('create', object)
+                        console.log(123)
+                    })
+            },
+            update(){
+                let needs = 'name summary'.split(' ')
+                let data = {}
+                needs.map((string)=>{
+                    data[string] = this.view.$el.find(`[name="${string}"]`).val()
+                })
+                this.model.update(data)
+                    .then(()=>{
+                        console.log(111)
+                        window.eventHub.emit('update',JSON.parse(JSON.stringify(this.model.data)))
+                    })
+            },
+            bindEvents(){
+                this.view.$el.on('submit','form',(e)=>{
+                    e.preventDefault();
+                    if(this.model.data.id){
+                        this.update()
+                    }else{
+                        this.create()
+                    }
+                })
+            },
+            bindEventHub(){
+                window.eventHub.on('selecePlaylist',(data)=>{
+                    this.model.data = data
+                    console.log(this.model.data)
+                    this.view.render(this.model.data)
+                })
+            }
+        }
+        controller.init(view,model)
+    }
---
+++ src/js/admin-playlists/playlists-list.js
@@ -0,0 +1,119 @@
+{
+    let view = {
+        el: '#playlists',
+        template: `
+            <ul class="playList">
+            </ul>
+        `,
+        render(data){
+            let $el = $(this.el)
+            $el.html(this.template)
+            let {playlists,selectedePlaylistId} = data
+            let liList = playlists.map((playlist)=>{
+                let $li = $('<li></li>').text(playlist.name).attr('data-playlist-id',playlist.id)
+                if(playlist.id === selectedePlaylistId){
+                    $li.addClass('active')
+                }
+                return $li
+            })
+            $el.find('ul').empty()
+            liList.map((domLi)=>{
+                $el.find('ul').append(domLi)
+            })
+        }
+    }
+    let model = {
+        data: {
+            playlists: [],
+            selectedePlaylistId: undefined,  
+        },
+        find(){
+            var query = new AV.Query('Playlist')
+            return query.find().then((playlists)=>{
+                this.data.playlists = playlists.map((playlist)=>{
+                    return {id:playlist.id, ...playlist.attributes}
+                })
+                return playlists
+            })
+        }
+    }
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            this.bindEvents()
+            this.bindEventHub()
+            this.getAllPlaylists()
+        },
+        getAllPlaylists(){
+            this.model.find().then(()=>{
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                let playlistId = e.currentTarget.getAttribute('data-playlist-id')
+                this.model.data.selectedePlaylistId = playlistId
+                this.view.render(this.model.data)
+                let data
+                let playlists = this.model.data.playlists
+                for(let i = 0;i<playlists.length;i++){
+                    if(playlists[i].id === playlistId){
+                        data = playlists[i]
+                        break
+                    }
+                }
+                window.eventHub.emit('selecePlaylist',JSON.parse(JSON.stringify(data)))
+            })
+        },
+        bindEventHub(){
+            window.eventHub.on('create', (playlistData) => {
+                this.model.data.playlists.push(playlistData)
+                this.view.render(this.model.data)
+            })
+            window.eventHub.on('update',(playlist)=>{
+                let playlists = this.model.data.playlists
+                for(let i = 0; i<playlists.length; i++){
+                    if(playlists[i].id === playlist.id){
+                        Object.assign(playlists[i], playlist)
+                    }
+                }
+                console.log(this.model.data)
+                this.view.render(this.model.data)
+            })
+        }
+    }
+    controller.init(view,model)
+}
+// // 微积分课程
+// var Playlist = AV.Object.createWithoutData('playList', '5b93982f17d0090034c73eec');
+
+// // 构建 StudentCourseMap 的查询
+// var query = new AV.Query('PlaylistMap');
+
+// // 查询所有选择了线性代数的学生
+// query.equalTo('playlist', Playlist);
+
+// // 执行查询
+// query.find().then(function (PlaylistMaps) {
+//     // studentCourseMaps 是所有 course 等于线性代数的选课对象
+//     // 然后遍历过程中可以访问每一个选课对象的 student,course,duration,platform 等属性
+//     PlaylistMaps.forEach(function (scm, i, a) {
+//         var playlist = scm.get('playlist');
+//         var author = scm.get('author')
+//         console.log(author)
+//     });
+// });
+
+
+// 构建关系表
+// var Playlist = AV.Object.createWithoutData('playList', '5b93a0311b69e6005b52855d')
+
+// var Song = AV.Object.createWithoutData('Song', '5b8f04e467f35600349a2f67')
+
+// var PlaylistMap = new AV.Object('PlaylistMap');
+
+// PlaylistMap.set('author', Song)
+// PlaylistMap.set('playlist', Playlist)
+
+// PlaylistMap.save()
